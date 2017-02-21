e786a83ecb9a251c7018d6b92215e60098897bed
Shahina <shahina@qburst.com>
2017-02-21 10:53:37 +0530

    Initial commit

---
+++ productCategories.js
@@ -0,0 +1,132 @@
+var sampleProductCategories = [
+  {
+    "products": [
+      {
+        "name": "iPhone 5s",
+        "price": 153.9
+      },
+      {
+        "name": "iPhone 6",
+        "price": 262.16
+      },
+      {
+        "name": "iPhone 6s",
+        "price": 347.25
+      },
+      {
+        "name": "iPhone 6 Plus",
+        "price": 298.58
+      }
+    ],
+    "name": "Apple",
+    "image": "images/Apple.png"
+  },
+  {
+    "products": [
+      {
+        "name": "Galaxy J5 ",
+        "price": 72.82
+      },
+      {
+        "name": "Galaxy S7",
+        "price": 98.3
+      },
+      {
+        "name": "Galaxy S7 edge",
+        "price": 69.93
+      },
+      {
+        "name": "Galaxy Note 4",
+        "price": 34.21
+      },
+      {
+        "name": "Galaxy J5",
+        "price": 15.91
+      },
+      {
+        "name": "Galaxy Note5 32GB",
+        "price": 32.33
+      },
+      {
+        "name": "Galaxy S6 edge 32GB",
+        "price": 103.42
+      },
+      {
+        "name": "Galaxy S5 32GB",
+        "price": 65.96
+      },
+      {
+        "name": "Galaxy Alpha",
+        "price": 29.18
+      },
+      {
+        "name": "Galaxy Note5 Dual Sim",
+        "price": 75.16
+      },
+      {
+        "name": "Galaxy Mega 5.8 I9152",
+        "price": 95.34
+      },
+      {
+        "name": "Galaxy Golden I9230",
+        "price": 50.51
+      }
+    ],
+    "name": "Samsung",
+    "image": "images/Samsung.png"
+  },
+  {
+    "products": [
+      {
+        "name": "1900s Vintage Bi-Plane",
+        "price": 34.25
+      },
+      {
+        "name": "1900s Vintage Tri-Plane",
+        "price": 36.23
+      },
+      {
+        "name": "1928 British Royal Navy Airplane",
+        "price": 66.74
+      },
+      {
+        "name": "1980s Black Hawk Helicopter",
+        "price": 77.27
+      },
+      {
+        "name": "ATA: B757-300",
+        "price": 59.33
+      },
+      {
+        "name": "America West Airlines B757-200",
+        "price": 68.8
+      },
+      {
+        "name": "American Airlines: B767-300",
+        "price": 51.15
+      },
+      {
+        "name": "American Airlines: MD-11S",
+        "price": 36.27
+      },
+      {
+        "name": "Boeing X-32A JSF",
+        "price": 32.77
+      },
+      {
+        "name": "Corsair F4U ( Bird Cage)",
+        "price": 29.34
+      },
+      {
+        "name": "F/A 18 Hornet 1/72",
+        "price": 54.4
+      },
+      {
+        "name": "P-51-D Mustang",
+        "price": 49.0
+      }
+    ],
+    "name": "Sony",
+    "image": "images/Sony.png"
+  }
+];
